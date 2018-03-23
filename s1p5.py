19b9627a3b6d206bc7b7519d867087e12a7fef7b
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-23 17:15:27 +0000

    Create s1p5.py

---
+++ s1p5.py
@@ -0,0 +1,16 @@
+a=input()
+
+b=input()
+
+c=input()
+
+if (a>b and a>c):
+  
+  print("a is the largest num")
+
+elif (b>a and b>c):
+    
+ print("b is the largest num")
+
+else:
+    print ("c is the largest num")
