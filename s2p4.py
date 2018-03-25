5ad736a7e44cc87888828a603b98619a05abfa48
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-25 08:55:38 +0530

    Update s2p4.py

--- s2p4.py
+++ s2p4.py
@@ -1,7 +1,7 @@
 def main():
-  a=int(input(""))
-  b=int(input(""))
-  for i in range(a+1,b+1):
+  x=int(input(""))
+  y=int(input(""))
+  for i in range(x+1,y+1):
     if(i%2!=0):
         print(i)
 if __name__ == '__main__':
