48b4b72b1eeadc4580f47f73e3253fab8157b390
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-26 20:09:09 +0530

    Create s3p4.py

---
+++ s3p4.py
@@ -0,0 +1,8 @@
+number=int(input("enter the length:"))
+print(number)
+list=[]
+for i in range(0,number):
+    newNo=int(input(''))
+    a=list.append(newNo)
+list.sort()
+print("Sorted list is",list)
