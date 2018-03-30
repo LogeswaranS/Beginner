e95ec47e2a96f9f4f3292408471f98691cda5324
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-30 18:58:27 +0530

    Create s6p10.py

---
+++ s6p10.py
@@ -0,0 +1,14 @@
+include<stdio.h>
+int main()
+{
+    int r,i,t1,t2=1,t3=0;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    for(i=0;i<r;i++)
+    {
+        t1=t2+t3;
+        t2=t3;
+    t3=t1;
+    printf("%d",t1);
+     }
+}
