c05397781e9aae98639afaf8e91d20edfc0c1721
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-30 20:48:37 +0530

    Create s7p2.py

---
+++ s7p2.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char ch[40];
+    int j;
+    scanf("%s",&ch);
+    if(ch[j]== '0' || ch[j]=='1')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
+
