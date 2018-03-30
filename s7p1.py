96ca1f8460db62729e4f1e4a6b646529ef505ef5
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-30 20:14:56 +0530

    Create s7p1.py

---
+++ s7p1.py
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10];
+    int j,k;
+    printf("Enter the string");
+    scanf("%s",&a);
+    printf("Enter the number of letters to be displayed");
+    scanf("%d",&n);
+    for(j=0;j<k;j++)
+    {
+        printf("%c",a[j]);
+    }
+
+    return 0;
+}
