3dfd874e9036b15e613152aead3cc17dc7838ef5
LogeswaranS <35947806+LogeswaranS@users.noreply.github.com>
2018-03-30 17:59:15 +0530

    Create s5p3.py

---
+++ s5p3.py
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	char x[20];
+	char y[20];
+	printf("enter the character:\n");
+	scanf("%s",&x);
+	printf("enter the next character:\n");
+	scanf("%s",&y);
+	printf("%s%s",x,y);
+}
